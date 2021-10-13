af53c2cccbe9f89e3eb0d947a25bf6ec4fcf20ab
vuthanhtung172906 <vuthanhtung172906@gmail.com>
2021-10-13 07:45:13 +0700

    Google Login

--- controllers/userCtr.ts
+++ controllers/userCtr.ts
@@ -1,14 +1,18 @@
 import { compare, hash } from 'bcrypt';
 import { Request, Response } from 'express';
+import { google } from 'googleapis';
+const { OAuth2 } = google.auth;
+const client = new OAuth2(process.env.MAIL_CLIENT_ID);
 import {
   generateAccessToken,
   generateActiveToken,
   generateRefreshToken,
 } from '../config/genarateToken';
-import { IDecodedToken } from '../config/interface';
+import { IDecodedToken, IUser } from '../config/interface';
 import sendEmail from '../config/sendMail';
 import jwt from 'jsonwebtoken';
 import Users from '../models/useModel';
+import { TokenPayload } from 'google-auth-library';
 const CLIENT_URL = `${process.env.BASE_URL}`;
 const useCtr = {
   register: async (req: Request, res: Response) => {
@@ -116,6 +120,68 @@ const useCtr = {
         });
       }
       res.status(200).json({ msg: 'Change profile success !' });
+    } catch (error) {
+      return res.status(500).json({ msg: (error as Error).message });
+    }
+  },
+  googleLogin: async (req: Request, res: Response) => {
+    try {
+      const { tokenId } = req.body;
+      const verify = await client.verifyIdToken({
+        idToken: tokenId,
+        audience: process.env.MAILING_SERVICE_CLIENT_ID,
+      });
+      const { email_verified, name, picture, email } = verify.getPayload() as TokenPayload;
+      const paswordhash = await hash(email as string, 12);
+      if (!email_verified) return res.status(400).json({ msg: 'Email verification failed.' });
+      const user = await Users.findOne({ email });
+      if (user) {
+        const isMatch = await compare(email as string, user.password);
+        if (!isMatch) return res.status(400).json({ msg: 'Password is incorrect.' });
+        const access_token = generateAccessToken({ id: user._id });
+        const refresh_token = generateRefreshToken({ id: user._id });
+
+        res.cookie('refreshtoken', refresh_token, {
+          httpOnly: true,
+          path: '/api/refreshtoken',
+          maxAge: 7 * 24 * 60 * 60 * 1000,
+        });
+        res.json({
+          msg: 'Login Success',
+          access_token,
+          user: {
+            ...user._doc,
+            password: '',
+          },
+        });
+      } else {
+        const newUser = new Users({
+          name: name,
+          email: email,
+          password: paswordhash,
+          avatar: {
+            public_id: '',
+            url: picture,
+          },
+        });
+        await newUser.save();
+        const access_token = generateAccessToken({ id: newUser._id });
+        const refresh_token = generateRefreshToken({ id: newUser._id });
+
+        res.cookie('refreshtoken', refresh_token, {
+          httpOnly: true,
+          path: '/api/refreshtoken',
+          maxAge: 7 * 24 * 60 * 60 * 1000,
+        });
+        res.json({
+          msg: 'Login Success',
+          access_token,
+          user: {
+            ...newUser._doc,
+            password: '',
+          },
+        });
+      }
     } catch (error) {
       return res.status(500).json({ msg: (error as Error).message });
     }
--- routes/userRoute.ts
+++ routes/userRoute.ts
@@ -10,4 +10,5 @@ router.post('/login', useCtr.login);
 router.get('/logout', useCtr.logout);
 router.post('/refreshtoken', useCtr.refreshToken);
 router.patch('/editprofile', auth, useCtr.changeInfoUser);
+router.post('/googlelogin', useCtr.googleLogin);
 export default router;
